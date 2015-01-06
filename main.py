3bba55f860843c7a47c5b279be52217e1e293f61
Shelby <vanhooser@ou.edu>
2015-01-05 17:23:13 -0800

    Added queuing

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ import serial, time, threading, Queue
 inQueue = Queue.Queue()
 outQueue = Queue.Queue()
 
+inSerial = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.001)
+outSerial = serial.Serial('/dev/ttyUSB1', 9600, timeout=0.001)
+
+inSerial.open()
+outSerial.open()
+
 def inFunc():
 	while True:
-		print 'Left'
-		time.sleep(5)
+		if not outQueue.empty():
+			outFrame = outQueue.get_nowait()
+			outSerial.write(outFrame)
+		readFrame = inSerial.read()
+		if readFrame:
+			inQueue.put(readFrame)
+		time.sleep(0.0001)
 
 def outFunc():
 	while True:
-		print 'right'
-		time.sleep(5)
+		if not inQueue.empty():
+			inFrame = inQueue.get_nowait()
+			inSerial.write(inFrame)
+		readFrame = outSerial.read()
+		if readFrame:
+			outQueue.put(readFrame)
+		time.sleep(0.0001)
 
 outThread = threading.Thread(target=inFunc)
 inThread = threading.Thread(target=outFunc)
